ea4bbfeee22fad8a76fe42481e3b259eb7ce7740
ycaptain <cz.ycaptain@gmail.com>
2023-12-19 04:04:17 +0800

    feat(fsm): problem fsm add idle state

--- src/store/problem.ts
+++ src/store/problem.ts
@@ -32,14 +32,14 @@ const defaultQuestion: Question = {
   reviews: [],
 } as const;
 
-/** @xstate-layout N4IgpgJg5mDOIC5QAcBOB7ARgGzAWwDoBLCXAYlgBcBDVSgbQAYBdRFdWIyo9AOzZAAPRAEYALIwIA2AKwAOObJkyATOLFSANCACeiAMwiAnAQDs+mY0bKZso-v1SAvk+1osuQpVTUivP1Bk3r68TKxIIMgcXDz8EcIIKjL6BGIiKqYKUo4qciL62nqJYmIEcqpy+moVUmkubhg4+ATBfgFkeNRUYKhhAlGc3HwCCY4iBCJSU6ZSeUYiInIqhQYlBEYyRlK5sipJS-WRjZ4tPm28gahwYAws-dFDcaAJ4pKyWTZqJVq6iHulGkYGUmtjkpkYUlMh3cTUInW6VwgQTOoTuEQGMWG8UQYiW0kqMlMpgqliBK2KpgIFgy8zE+g0eWs0OOzXhlB6kDIV1gNz66IesRGBmyVLSIlMRkYDmsdPJeSpe0lYnB4MY9jELlcIF46AgcH6LLw90GguxCAAtD8iuaZOsjPaHY6HfpmR5miRcMbMU8hDjlr8ENKCJYZMZtqYMoYoVqYSdWv4Ll7HkLEqHg+DDOl0hlQ1bVrb0jMrBt9B8NTHDQQ2RyIEnTc8caZSkl9kYJYtypNyfTKeGpMXkh9NU4gA */
+/** @xstate-layout N4IgpgJg5mDOIC5QAcBOB7ARgGzAWwDp9kAXATwGJYSBDVEgbQAYBdRFdWASxK-QDt2IAB6IAjEzEEAbGIBMcgCwBmAOxiAnBtUAOOaoA0IMojk7FBcwFYNTaeYlM5ygL4ujaLLkJcIuCiSoNFz8zGxIIMicPHyCEaIIijoaBEzJuqrSytJWTJJGJgg6VgRy0hpWykyZtopMimJuHhg4+ASBwfwhUAFBIWFCUdy8AkIJYtKqlnkainJWVnNlTMoFiMraBKoKYio6qjYKZU2RLd7tfV38PXg01GCoAxFDMaPx65MEVts69rN18nMawQyjEU1B+3Uc0UKiqqhOnlahA6IW6FFQcDAjFYg2iIzioASSRSaW0snMaQaE2BMOUpWkK12NgWOmUcgRZzat3uGIgvU6Tw4w1iY0QxNS6XJdQcYmBcgkBDEoLMVnlWRyrncpy8XLuJAekHRmOx4SFrwJIkQDJ0Mk0yjZqlm2hWwKqcgIymhKx0elUVV2bi1-HQEDgg05eFxwrehMQAFppBYNvYGRpzBp5RpZcZ4+6mJU1Dpqmy6p7vhydYRiOQo+bRQg4xMPRoU0w04oM5oxFZXdVSspzFl5Kr6hpNc1KwRfLha-j6366bk5Gl80X-TpgWJNKUVuUKmoJBJFBWkRdOt1ZyL3ghnNJUvo6nItPZdBVXU205Mn6oVisT+duX1XlLxjS1EiLGQrH2Iss30NRpFdJQtk9Z8zDSBYwUDFwgA */
 export const problemMachine = createMachine(
   {
-    initial: "idle",
+    initial: "empty",
     id: "problem",
     context: JSON.parse(JSON.stringify(defaultQuestion)),
     states: {
-      idle: {
+      empty: {
         on: {
           start: {
             target: "training",
@@ -50,6 +50,16 @@ export const problemMachine = createMachine(
                 ...event.question,
                 reviews: [now],
               } satisfies Question
+            }),
+          },
+        },
+      },
+      idle: {
+        on: {
+          train: {
+            target: "training",
+            actions: assign({
+              reviews: (ctx) => [...ctx.context.reviews, Date.now()]
             }),
           },
         },
